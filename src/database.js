5725b9b4ffb5b9c61f15a080686af97af34425d9
Marcos2x2k <marcos2x2k@hotmail.com>
2022-10-26 12:53:19 -0300

    fist commit

--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://inspecobras', {
-    // mongoose.connect(process.env.MONGODB_URI, { 
+// mongoose.connect('mongodb://localhost/inspecobras', {
+    mongoose.connect('mongodb://localhost/Inspecobrasctes', {
+// mongoose.connect(process.env.MONGODB_URI, { 
     // useCreateIndex: true,
     useNewUrlParser: true,
     // useFindAndModify: false
-})
+    })
     .then(db => console.log('Base de Datos Conectada'))
     .catch(err => console.error(err));
 
