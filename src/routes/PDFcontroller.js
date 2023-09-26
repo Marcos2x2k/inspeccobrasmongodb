62a970145966c4557ec466a2ce498490062adffc
Marcos2x2k <Marcos2x2k@hotmail.com>
2023-09-26 13:50:23 -0300

    listoparaactualizar 26-9-23

--- src/routes/PDFcontroller.js
+++ src/routes/PDFcontroller.js
@@ -1,6 +1,8 @@
-module.exports = {
-    
-factura (req, res) {
+const express = require('express');
+const router = express.Router();
+
+function factura (req, res) {
     res.render('view/pdfs/factura', { layouts: "pdf"})
 }
-}
+
+module.exports = router;
